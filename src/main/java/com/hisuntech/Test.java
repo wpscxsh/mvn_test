f89ed89393a0c2cce27a9e5d61dfb1d6b58b766f
miller <13476298420@139.com>
2019-06-21 13:21:20 +0800

    11

--- src/main/java/com/hisuntech/Test.java
+++ src/main/java/com/hisuntech/Test.java
@@ -6,7 +6,8 @@ package com.hisuntech;
  */
 public class Test {
 
+
     public static void main(String[] args) {
-        System.out.println("-222ss------------") ;
+        System.out.println("-2ss22ss------------") ;
     }
 }
